011be788abc0cae49be9cc4deda863ca92949442
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-28 12:56:51 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  manage_aws_auth_configmap = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
+# VPC Module (creates networking for EKS)
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0"
+
+  name = "poc-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
