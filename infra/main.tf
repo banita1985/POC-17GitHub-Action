7fc457857d6c7e5476f05fe53e37c33543172e78
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-08-28 13:13:33 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,33 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0"
+
+  name = "poc-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 20.0"
@@ -18,20 +48,4 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
-}
-
-# VPC Module (creates networking for EKS)
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0"
-
-  name = "poc-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
 }
